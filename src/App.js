a348e96e801d17b9acf54767d6641e847132079e
Sachin <sachin.kumar01@digimantra.com>
2022-09-03 15:36:40 +0530

    Initail Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { firebaseDefaultApp } from "./config";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Demo App
+      {console.log(firebaseDefaultApp)}
+    </div>
+  );
+}
+
+export default App;
